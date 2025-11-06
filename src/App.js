b1ff199b62df5070b38e11e29c73d5dd77f6866e
vader138 <cooperjulien10@gmail.com>
2025-11-06 14:26:05 -0600

    added dark mode switch that does nothing

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap';
-import {Dropdown, Nav, Navbar, NavDropdown, Container, Tab, Tabs, Carousel, Card} from 'react-bootstrap';
+import {
+  Dropdown,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Container,
+  Tab,
+  Tabs,
+  Carousel,
+  Card,
+  ToggleButton,
+  Form
+} from 'react-bootstrap';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import anim from "./assets/deltaprophecy-animation.gif";
+import {useState} from "react";
 function App() {
+  const [darkmode, setDarkmode] = useState(false);
   return (
       <div className="App">
+        <div style={{position: "relative"}}>
+          <Form>
+            <Form.Check type="switch" id="darkModeSwitch" label="Dark Mode" style={{position: "absolute", top: 10, right: 10, zIndex: 9999}} onClick={() => setDarkmode(!darkmode)}/>
+          </Form>
+        </div>
         <Tabs defaultActiveKey="home" id="uncontrolled-tab-example" className="mb-3">
           <Tab eventKey="home" title="Home">
             <div>
-              <h1>Welcome to my website!</h1>
-              <h2>I'm gonna be honest, I have no idea what to put here yet.</h2>
-              <h3>This is some placeholder text until I work out what to put in the home page.</h3>
-              <h4>Here's the entire <i>Undertale</i> intro.</h4>
-              <p>A long time ago, two races lived on Earth: <i>Humans</i> and <i>Monsters</i>.</p>
-              <p>One day, war broke out between the two races.</p>
-              <p>After a long battle, the humans were victorious.</p>
-              <p>They sealed the monsters underground with a magic spell.</p>
-              <h6>Mount Ebbot</h6>
-              <h6>201X</h6>
-              <p>Legend says that those who climb the mountain never return.</p>
+              <h1>HOME PAGE</h1>
             </div>
           </Tab>
           <Tab eventKey="profile" title="Profile">
             <div>
-              <h2>About Me</h2>
-              <p>This is where I'll introduce myself, once this website is finished.</p>
+              <h1>ABOUT ME</h1>
             </div>
           </Tab>
           <Tab eventKey="contact" title="Portfolio">
             <div>
-              <h2>Portfolio</h2>
-              <p>Here's where I'd put some info on projects I've done in the past.</p>
-              <p>You know, if I had any.</p>
-              <p>For now, this page will probably just have a link to my Github.</p>
+              <h1>PORTFOLIO</h1>
             </div>
           </Tab>
         </Tabs>
